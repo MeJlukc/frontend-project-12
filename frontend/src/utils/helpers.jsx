d173ffadd0e009353d536219f65faddd8ef60019
MeJlukc <solomatin.yarik@mail.ru>
2025-10-14 16:17:57 +0300

    updated utils/

---
+++ frontend/src/utils/helpers.jsx
@@ -0,0 +1,40 @@
+import { AUTH_ERRORS, UI_ERRORS } from './constants'
+
+export const extractAuthError = status => AUTH_ERRORS[status]
+
+export const extractUiError = (status) => {
+  if (extractAuthError(status)) return undefined
+
+  return UI_ERRORS[status] || UI_ERRORS.Unknown
+}
+
+export const clearError = (state) => {
+  Object.assign(state, {
+    error: '',
+    isError: false,
+    redirectToLogin: false,
+  })
+}
+
+export const createErrorHandler = extractor => (state, { payload, meta }) => {
+  const status = payload?.status
+  const endpointName = meta?.arg?.endpointName || ''
+  const authEndpoints = ['login', 'signup']
+
+  if (!authEndpoints.includes(endpointName) && status === 401) {
+    clearError(state)
+    state.redirectToLogin = true
+    return
+  }
+
+  const error = extractor(status)
+
+  if (!error) {
+    clearError(state)
+    return
+  }
+
+  state.error = error
+  state.isError = true
+  state.redirectToLogin = false
+}
